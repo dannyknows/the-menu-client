bd78b2ebccb111dbcaa480d1a7670a41ec71486f
Danny_knows <dannylow456@gmail.com>
2020-07-22 13:51:15 +1000

    FEATURE: added protected routes for user dashboard and 404 page

---
+++ src/components/noMatch.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const NoMatch = () => {
+  return (
+    <>
+      <h1>404</h1>
+      <h1>page not found</h1>
+    </>
+  )
+};
+
+export default NoMatch;
---
+++ src/components/user/protectedRoute.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import Dashboard from "./dashboard";
+import Buffer from "../buffer";
+
+class ProtectedRoute extends Component {
+  state = {
+    auth: false,
+    loading: true,
+  };
+
+  componentDidMount() {
+    const token = localStorage.getItem("token");
+    if (token === "password") {
+      this.setState({
+        auth: true,
+        loading: false,
+      });
+    } else {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  render() {
+    const { loading, auth } = this.state;
+    if (!loading && !auth) {
+      return <Redirect to="/" />;
+    } else if (!loading && auth) {
+      return <Dashboard />;
+    } else {
+      return <Buffer />;
+    }
+  }
+}
+
+export default ProtectedRoute;
