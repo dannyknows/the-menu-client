ca620d813626c7cae64eb56c9557b362c4af5b60
Daniel Ask <cdanielask@gmail.com>
2020-07-24 14:14:41 +1000

    FEATURE: Added data fetching for restaurant pages

--- src/components/user/dashboard.jsx
+++ src/components/user/dashboard.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 export default class Dashboard extends Component {
+  async componentDidMount() {
+    const response = await fetch("http://localhost:3000/restaurants",  {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+    const restaurants = await response.json();
+    console.log(restaurants);
+  }
+
   render() {
     return (
       <>
