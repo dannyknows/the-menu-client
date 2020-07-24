aee57ae3c81551d55fd69c84005f4deaed3c1e57
Danny_knows <dannylow456@gmail.com>
2020-07-24 14:21:55 +1000

    FEATURE: added item styled components and logic

--- src/components/restaurant/menu.jsx
+++ src/components/restaurant/menu.jsx
@@ -1,24 +1,26 @@
 import React, { Component } from "react";
 
 // components
-import Item from "./item";
+import Bold from "./styledComponents/bold";
 
 export default class Menu extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-// className={theme.theme_class}
   render() {
     const { title, items, styles, theme } = this.props.menu;
-    console.log(this.props)
+    console.log(this.props);
     return (
-      <div  id={`${title}`}>
+      <div id={`${title}`}>
         <h1>{title}</h1>
         <div>
           {items &&
             items.map((item) => {
-              return <Item item={item} />
+              // switch (item.theme.theme_class) {
+              // UNCOMMENT FOR PRODUCTION
+              switch ("bold") {
+                case "bold":
+                  return <Bold item={item} />;
+                default:
+                  return "default";
+              }
             })}
         </div>
       </div>
