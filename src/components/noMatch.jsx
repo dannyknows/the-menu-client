5dcb325633013520b5fe3dd5916871038bffc288
Danny_knows <dannylow456@gmail.com>
2020-07-30 10:34:13 +1000

    FEATURE: 404 styles

--- src/components/noMatch.jsx
+++ src/components/noMatch.jsx
@@ -1,12 +1,16 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NoMatch = () => {
   return (
-    <>
-      <h1>404</h1>
-      <h1>page not found</h1>
-    </>
-  )
+    <div className={"noMatch"}>
+      <div>
+        <h3>404</h3>
+        <h1>page not found</h1>
+        <Link id="link" to={"/"}>Take me home!</Link>
+      </div>
+    </div>
+  );
 };
 
 export default NoMatch;
